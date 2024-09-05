1dd59e965dadab96d1c51f7930dbbd9d8cad8227
JPMoura <jp7401301@gmail.com>
2024-09-05 09:35:25 -0300

    add arquivos

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let xbolinha = 400
+let ybolinha = 200
+let diametrodabolinha = 22
+function setup() { 
+  createCanvas(800, 400); 
+}
+
+function draw() { 
+  background(0); 
+  circle(xbolinha,ybolinha,diametrodabolinha) //circle(x,y,d) x=cordenada x,y=cordenada y e d=diametro do circulo } 
+  //11:52 do video criando a bolinha e cenário
