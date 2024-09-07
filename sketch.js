a116cba292f8ed217a9eb629f8dc457fbbb364b6
JPMoura <jp7401301@gmail.com>
2024-09-07 01:11:40 -0300

    Update sketch.js
    colocando movimento e adicionando rebote nas bordas

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
-let xbolinha = 400
-let ybolinha = 200
-let diametrodabolinha = 22
+let xbolinha = 400;
+let ybolinha = 200;
+let diametrodabolinha = 22;
+let velocidadexdabolinha = 6;
+let velocidadeydabolinha = 6;
 function setup() { 
   createCanvas(800, 400); 
 }
 
 function draw() { 
   background(0); 
-  circle(xbolinha,ybolinha,diametrodabolinha) 
+  circle(xbolinha,ybolinha,diametrodabolinha);
+  xbolinha += velocidadexdabolinha;
+  ybolinha += velocidadeydabolinha;
+  if(xbolinha > width || xbolinha < 0){
+    velocidadexdabolinha*=-1;
+  }
+  if(ybolinha > height || ybolinha < 0){
+    velocidadeydabolinha*=-1;
+  }
 }
-  //circle(x,y,d) x=cordenada x,y=cordenada y e d=diametro do circulo 
