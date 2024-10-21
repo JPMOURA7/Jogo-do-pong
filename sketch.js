70989e8d5df415c8a8759f619baa8e735af9daef
JPMoura <jp7401301@gmail.com>
2024-10-21 11:18:16 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -141,11 +141,11 @@ function draw() {
     textSize(16);
     stroke("white")
     fill("blue");
-    rect (278,10,40,20);
-    rect (378,10,40,20);
+    rect (288,10,40,20);
+    rect (388,10,40,20);
     fill(255);
-    text(meuspontos,298,26);
-    text(pontosdooponente,398,26);
+    text(meuspontos,308,26);
+    text(pontosdooponente,408,26);
   }
   function marcarpontos(){
     if (xbolinha + raio > 800){
